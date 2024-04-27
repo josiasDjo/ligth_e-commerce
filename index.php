d491aa083d6ef95c7fef6d3571c5b38656365ca6
Besodia kabambi <138811540+djodv@users.noreply.github.com>
2024-04-27 19:40:43 +0200

    nav1

--- index.php
+++ index.php
@@ -3,9 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="dist/output.css">
     <title>Document</title>
 </head>
 <body>
-    
+    <?php  
+        require_once dirname(__DIR__) .  DIRECTORY_SEPARATOR . 'e-commerce' . DIRECTORY_SEPARATOR . 'pages' . DIRECTORY_SEPARATOR . 'navbarUti.php';
+    ?>
 </body>
 </html>
---
+++ pages/navbarUti.php
@@ -0,0 +1,9 @@
+<div class="navbarUti">
+    <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Explore</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="">Contact</a></li>
+        <li><a href="">About</a></li>
+    </ul>
+</div>
