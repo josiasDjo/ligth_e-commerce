61f3634e898d0ef87855d44cf89db3b4dcdfe885
Besodia kabambi <138811540+djodv@users.noreply.github.com>
2024-04-27 20:46:13 +0200

    tAcc

--- pages/index.php
+++ pages/index.php
@@ -4,11 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../dist/output.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Document</title>
 </head>
 <body>
     <?php  
         require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'pages' . DIRECTORY_SEPARATOR . 'navbarUti.php';
     ?>
+
+    <div class="accueil">
+        <i class="fa-solid fa-chevron-left"></i>
+        <i class="fa-solid fa-chevron-right"></i>
+    </div>
+
+    <div class="container_accueil">
+        <h1>OUR PRODUCTS</h1>
+        <div class="container">
+            <div class="sub-container">
+                <div class="nom">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+                <div class="explore">
+                    <a href="#"><button>Explore</button></a>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
---
+++ pages/navbarAd.php
@@ -0,0 +1,10 @@
+<div class="navbarUti">
+    <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Explore</a></li>
+        <li><a href="">Messages</a></li>
+        <li><a href="">Add new Articles</a></li>
+        <li><a href="">Settings</a></li>
+        <li><a href=""><button>Sign in</button></a></li>       
+    </ul>
+</div>
