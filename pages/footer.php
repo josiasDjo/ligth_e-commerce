da893d9b33dced6716e67a6e7b2bd142b58fa829
Besodia kabambi <138811540+djodv@users.noreply.github.com>
2024-05-06 15:11:52 +0200

    footer

--- pages/footer.php
+++ pages/footer.php
@@ -2,13 +2,26 @@
     <div class="icon-reseau">
         <!-- icon réseaux  -->
 
-        <p>Lorem ipsum dolor sit amet consectetur </p>
+        <i class="fa-brands fa-square-x-twitter"></i>
+        <i class="fa-brands fa-square-instagram"></i>
+        <i class="fa-brands fa-facebook"></i>
+
     </div>
 
     <div class="contact">
         <!-- contact et localisation -->
-
-        <p>Lorem ipsum dolor sit amet consectetur</p>
+        <ul>
+            <li><i class="fa-solid fa-location-dot"></i></li>
+            <li>DRC, Nord-Kivu, Goma</li>
+        </ul>
+        <ul>
+            <li><i class="fa-solid fa-phone"></i></li>
+            <li>+243 992 242 409</li>
+        </ul>
+        <ul>
+            <li><i class="fa-solid fa-message"></i></li>
+            <li>youremail@gmail.com</li>
+        </ul>
 
     </div>
     <div class="tous-droit">
