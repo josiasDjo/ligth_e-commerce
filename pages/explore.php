b3fc645a1c96d8c300515e7adcffa90977c0d48e
Besodia kabambi <138811540+djodv@users.noreply.github.com>
2024-05-22 19:17:29 +0200

    change7 explore

--- pages/explore.php
+++ pages/explore.php
@@ -15,22 +15,22 @@
 
         <div class="container">
 
-            <div class="panelCont">
+            <div class="panelCont" id="tab1">
                 <?php
                     require_once dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'ligth_e-commerce' . DIRECTORY_SEPARATOR . 'admin' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'phone.php';
                 ?>
             </div>
-            <div class="panelCont">
+            <div class="panelCont" id="tab2">
                 <?php
                     require_once dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'ligth_e-commerce' . DIRECTORY_SEPARATOR . 'admin' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'watch.php';
                 ?>
             </div>
-            <div class="panelCont">
+            <div class="panelCont" id="tab3">
                 <?php
                     require_once dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'ligth_e-commerce' . DIRECTORY_SEPARATOR . 'admin' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'airpod.php';
                 ?>
             </div>
-            <div class="panelCont">
+            <div class="panelCont" id="tab4">
                 <?php
                     require_once dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'ligth_e-commerce' . DIRECTORY_SEPARATOR . 'admin' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'laptop.php';
                 ?>
--- pages/navEplore.php
+++ pages/navEplore.php
@@ -1,8 +1,8 @@
 <div class="navExplore">
     <ul>
-        <li><button>WATCH</button></li>
-        <li><button>PHONE</button></li>
-        <li><button>AIRPOD</button></li>
-        <li><button>LAPTOP</button></li>
+        <li><button onclick="showTab('tab1')">WATCH</button></li>
+        <li><button onclick="showTab('tab2')">PHONE</button></li>
+        <li><button onclick="showTab('tab3')">AIRPOD</button></li>
+        <li><button onclick="showTab('tab4')">LAPTOP</button></li>
     </ul>
 </div>
