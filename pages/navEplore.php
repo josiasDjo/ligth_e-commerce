8be943a0cbb2eeb04d23c7167cf0f0298d8eb7a3
Besodia kabambi <138811540+djodv@users.noreply.github.com>
2024-05-22 20:00:43 +0200

    change18 explore

--- pages/navEplore.php
+++ pages/navEplore.php
@@ -1,7 +1,7 @@
 <div class="navExplore">
     <ul>
-        <li><button onclick="showTab('tab1')">WATCH</button></li>
-        <li><button onclick="showTab('tab2')">PHONE</button></li>
+        <li><button onclick="showTab('tab1')">PHONE</button></li>
+        <li><button onclick="showTab('tab2')">WATCH</button></li>
         <li><button onclick="showTab('tab3')">AIRPOD</button></li>
         <li><button onclick="showTab('tab4')">LAPTOP</button></li>
     </ul>
