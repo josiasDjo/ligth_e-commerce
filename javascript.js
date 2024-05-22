97317351b6005bff2e3c4cbfc0d20d56e07f3e46
Besodia kabambi <138811540+djodv@users.noreply.github.com>
2024-05-22 20:16:27 +0200

    change22 explore

--- javascript.js
+++ javascript.js
@@ -59,6 +59,6 @@ function showTab(tabId) {
     for (var i = 0; i < tabs.length; i++) {
         tabs[i].style.display = 'none';
     }
-    document.getElementById(tabId).style.display = 'block';
+    document.getElementById(tabId).style.display = 'flex-wrap';
 }
 
