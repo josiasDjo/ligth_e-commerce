7e4d77ee0756a5d94c34a46956311c18d4f16009
Besodia kabambi <138811540+djodv@users.noreply.github.com>
2024-05-26 15:27:15 +0200

    change7

--- javascript.js
+++ javascript.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 setTimeout(() => {
                     element.style.opacity = 1;
                     element.style.transform = 'translateY(0)';
-                }, index * 30); 
+                }, index * 15); 
             }
         });
     }
